a46181ec28019c656be7bbaa08720d3e33c6d98d
Epoc <contact@epoc.fr>
2019-08-15 15:47:01 +0200

    Add vigenère 1

--- crypto/vigenere_1.py
+++ crypto/vigenere_1.py
@@ -1,15 +1,17 @@
 # pip install pycipher
 
 from microcontest import Communicator
-from pycipher import Caesar
+from pycipher import Vigenere
 
 mc = Communicator.create_from_env()
 
-challenge_id = 4
+challenge_id = 10
 
 variables = mc.get_contest_variables(challenge_id)
 
-txt_clair = Caesar(int(variables['key'])).decipher(variables['txt_crypte'])
+txt_clair = Vigenere(variables['clef']).decipher(variables['txt_crypte'])
+
+print(txt_clair)
 
 data = {'txt_clair': txt_clair}
 
